864f4408e19177dd11277290bfad53b9ec2d4f32
Athapaththu <99331500+sakilaathapaththu@users.noreply.github.com>
2025-06-26 17:52:04 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:10000";
-  //export const API_BASE_URL = "https://myportfolio-xi-navy.vercel.app";
+//export const API_BASE_URL = "http://localhost:10000";
+export const API_BASE_URL = "https://myportfolio-xi-navy.vercel.app/";
